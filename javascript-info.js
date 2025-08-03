be367a1579e63365a33cc2bcb5dbaa3eec56d444
Alberto <albi.web@gmail.com>
2025-08-03 23:23:34 +0200

    Added some exercisees from javascript.info

---
+++ javascript-info.js
@@ -0,0 +1,92 @@
+function alert(param) {
+  console.log(param);
+}
+/* 
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [john, pete, mary];
+
+let names = users.map((user) => user.name);
+
+alert(names); // John, Pete, Mary
+ */
+/* 
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [john, pete, mary];
+
+let usersMapped = users.map((item) => {
+  let obj = { fullName: `${item.name} ${item.surname}`, id: item.id };
+  return obj;
+});
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+
+
+alert(usersMapped[0].id); // 1
+alert(usersMapped[0].fullName); // John Smith */
+
+/* function sortByAge(array) {
+  array.sort((a, b) => a.age - b.age);
+}
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [pete, john, mary];
+
+sortByAge(arr);
+
+// now: [john, mary, pete]
+alert(arr[0].name); // John
+alert(arr[1].name); // Mary
+alert(arr[2].name); // Pete */
+
+/* function getAverageAge(arr) {
+  return arr.reduce((acc, val) => acc + val.age, 0) / arr.length;
+}
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [john, pete, mary];
+
+alert(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
+ */
+
+function groupById(users) {
+  let obj = {};
+  users.forEach((element) => {
+    obj[element.id] = element;
+  });
+  return obj;
+}
+
+let users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
+];
+
+let usersById = groupById(users);
+console.log(usersById);
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
